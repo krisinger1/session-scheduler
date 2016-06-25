d5e25f38bbcce3c1099dcd699fc7afd996d8bae1
Kristin <Kristin@COOL-BLUE>
2016-06-24 21:42:48 -0500

    added javadoc comments to Tree class and commented out some apparently unused functions

--- PrepSessionScheduler/src/org/intervarsity/Tree.java
+++ PrepSessionScheduler/src/org/intervarsity/Tree.java
@@ -4,7 +4,11 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.LinkedList;
-
+/**
+ * A Tree of possible session options
+ * @author Kristin
+ *
+ */
 public class Tree {
 	LinkedList<Tree> leaves;
 	Tree parent=null;
@@ -18,28 +22,44 @@ public class Tree {
 		leaves=new LinkedList<Tree>();
 	}
 	
+	/**
+	 * Determines if this node has any leaves
+	 * @return true if there is at least one leaf, false otherwise
+	 */
 	public boolean hasLeaves(){
 		if (leaves.size()>0) return true;
 		else return false;
 	}
 	
+	/**
+	 * Adds a leaf to this node
+	 * @param leaf the leaf to add to the tree at this node
+	 */
 	public void addLeaf(Tree leaf){
 		leaves.add(leaf);
 	}
 	
+	/**
+	 * removes leaf from this node and recursively removes all of leaf's leaves
+	 * @param leaf the leaf to remove from tree
+	 */
 	public void removeLeaf(Tree leaf){
-		// TODO make sure leaf exists for this parent
-		
-		
-		while (leaf.hasLeaves() ){
-			leaf.removeLeaf(leaf.leaves.get(0));
-		}
-		leaves.remove(leaf);
+		if (this==leaf.parent){		// make sure this leaf exists for this parent
+			while (leaf.hasLeaves() ){
+				leaf.removeLeaf(leaf.leaves.get(0));
+			}
+			leaves.remove(leaf);
 
-		//System.out.println("********");
-		//printTree(this, 0);
+			//System.out.println("********");
+			//printTree(this, 0);
+		}
 	}
 	
+	/**
+	 * Determines if all leaves of this node are dead - 
+	 * meaning they do not provide a viable solution
+	 * @return true if all leaves are dead, otherwise false
+	 */
 	private boolean allLeavesDead(){
 		for (Tree leaf:leaves){
 			if (!leaf.isDead) return false;
@@ -47,6 +67,10 @@ public class Tree {
 		return true;
 	}
 	
+	/**
+	 * Removes branches from tree that do not lead to usable solutions
+	 * 
+	 */
 	public void pruneTree(){
 		if (isEnd) return;
 		int i=0;
@@ -72,7 +96,10 @@ public class Tree {
 		
 	}
 	
-	public void printLeaves(){
+	/**
+	 * Prints to console all descendants of this node with session info
+	 */
+	/*public void printLeaves(){
 		for (Tree t:leaves){
 			t.session.print();
 			System.out.println("");
@@ -83,14 +110,21 @@ public class Tree {
 				t.printLeaves();
 			}
 		}
-	}
-	
-	public void print(){
+	}*/
+	/**
+	 * Prints to console the entire tree of solutions with session info
+	 */
+	/*public void print(){
 		System.out.println("The Tree");
 		if (session!=null) {System.out.print("node");session.print();}
 		printLeaves();
-	}
+	}*/
 	
+	/**
+	 * Prints to console entire tree in a readable way
+	 * @param t the Tree object to print
+	 * @param indent how far to indent next level of tree
+	 */
 	public static void printTree (Tree t, int indent) {
 	    StringBuilder ind = new StringBuilder();
 	    for (int i = 0; i < indent; i++)
@@ -112,6 +146,13 @@ public class Tree {
 	    }
 	}
 	
+	/**
+	 * Prints to file entire tree in a readable way
+	 * used by printSolutionToFile
+	 * @param t the Tree object to print
+	 * @param indent how far to indent next level of tree
+	 * @param writer PrintWriter object
+	 */
 	private static void printTreeToFile (Tree t, int indent, PrintWriter writer) {
 	    StringBuilder ind = new StringBuilder();
 	    for (int i = 0; i < indent; i++)
@@ -133,6 +174,12 @@ public class Tree {
 	    }
 	}
 	
+	/**
+	 * Prints solutions tree to file specified in filename
+	 * @param t solution tree to print
+	 * @param indent how far to indent next level of tree
+	 * @param filename file to print to
+	 */
 	public static void printSolutionToFile(Tree t,int indent,String filename){
 		PrintWriter writer;
 		try {
@@ -148,7 +195,8 @@ public class Tree {
 		}
 	}
 	
-	private static String printRankedSolution(Tree t, String solution){
+	//TODO is this code even correct for anything? Looks like infinite recursion
+	/*private static String printRankedSolution(Tree t, String solution){
 		if(t.isEnd){
 			solution+="\t"+t.session.time+"\n";
 			return solution;
@@ -174,5 +222,5 @@ public class Tree {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
+	}*/
 }
